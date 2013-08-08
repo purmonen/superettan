0060bdfe8fe5714c86475e855a98f326bebbf704
purmonen <purmonen@kth.se>
2013-08-08 11:19:27 +0200

    Added logos

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 teams = [];
+logos = {};
+logos.positions = [
+    'ängelholms ff', 'assyriska ff', 'östersunds fk', 'ik brage',
+    '', 'degerfors if', 'falkenbergs ff', 'hammarby if',
+    'jönköpings södra if', 'landskrona bois', 'ljungskile', '',
+    'örgryte is', 'gif sundsvall', 'ifk värnamo', 'örebro sk', '',
+    'gais', '', 'varbergs bois fc'  
+];
 
+logos.width = 30;
 
 function getTeams() {
     $.ajax({
         url: 'read.php',
         success: function (res) {
             teams = [];
-            positions = ['lag', 'm', 'v', 'o', 'f', 'gm-im', '+/-', 'p']; 
-
+            positions = ['lag', 'm', 'v', 'o', 'f', 'gm-im', '+/-', 'p'];
             table = $(res).find('.matchtable').first();
             $.each(table.find('tr'), function() {
                 var row = $(this);
@@ -16,8 +24,8 @@ function getTeams() {
                     var column = $(this);
                     if (index < positions.length) {
                         if (positions[index] === 'gm-im') {
-                            team['gm'] = column.text().split('-')[0];
-                            team['im'] = column.text().split('-')[1];
+                            team.gm = column.text().split('-')[0];
+                            team.im = column.text().split('-')[1];
                         } else {
                             team[positions[index]] = column.text();
                         }
@@ -38,8 +46,8 @@ function createTable(teams, sortKey) {
     }
 
     teams.sort(function(first, second) {
-        a = parseInt(first[sortKey]);
-        b = parseInt(second[sortKey]);
+        a = parseInt(first[sortKey], 10);
+        b = parseInt(second[sortKey], 10);
         if (isNaN(a)) {
             a = first[sortKey];
             b = second[sortKey];
@@ -71,7 +79,7 @@ function createTable(teams, sortKey) {
         if (key === sortKey) {
             focusedColumn = column;
             column.addClass('th-selected');
-        }
+         }
         function update() {
             table.remove();
             createTable(teams, key);
@@ -98,7 +106,27 @@ function createTable(teams, sortKey) {
         position.text(index+1);
         $.each(this, function(key, value) {
             var column = $('<td></td>');
-            column.text(value);
+            var name = $('<p/>');
+            name.css({
+                'display': 'inline-block',
+                'margin': '8px 0px'
+            });
+            name.text(value);
+            column.append(name);
+            if (key === 'lag') {
+                var left = -logos.width * logos.positions.indexOf(
+                    value.toLowerCase().trim());
+                var logo = $('<div></div>');
+                column.prepend(logo);
+                logo.css({
+                    float: 'left',
+                    width: '30px',
+                    margin: '0 5px 0 0',
+                    height: '27px',
+                    'background-image': 'url(logos.png)',
+                    'background-position': left + 'px 0px',
+                });
+            }
             row.append(column);
             if (key === sortKey) {
                 column.addClass('td-selected');
